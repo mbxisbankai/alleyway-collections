b23b9b4dcbb0c7c46fbafc4e18caa0080f0bae4a
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-07-02 13:02:56 +0300

    Edited config.py

--- server/config.py
+++ server/config.py
@@ -7,17 +7,24 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = True
-    PERMANENT_SESSION_LIFETIME = timedelta(days=1) 
+    PERMANENT_SESSION_LIFETIME = timedelta(days=1)
     SESSION_USE_SIGNER = True
     SESSION_REDIS = redis.from_url(os.getenv("REDIS_URL"))
     SESSION_COOKIE_NAME = "alleyway_session"
-    SESSION_COOKIE_SAMESITE = "None"    
-    SESSION_COOKIE_SECURE = True  
     SESSION_KEY_PREFIX = "alleyway_session:"
     SESSION_COOKIE_HTTPONLY = True
+
+    # Handle secure cookies only in production
+    if os.getenv("FLASK_ENV") == "production":
+        SESSION_COOKIE_SAMESITE = "None"
+        SESSION_COOKIE_SECURE = True
+    else:
+        SESSION_COOKIE_SAMESITE = "Lax"
+        SESSION_COOKIE_SECURE = False
+
+
